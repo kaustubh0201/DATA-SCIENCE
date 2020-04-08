3f533cc2de5091db5ac1432ddb09f3d334b009fe
kaustubh0201 <55658401+kaustubh0201@users.noreply.github.com>
2020-04-09 02:38:52 +0530

    Delete fifth_np.py

--- fifth_np.py
+++
@@ -1,7 +0,0 @@
-import numpy as npy
-
-arr1 = npy.array([[5, 6, 9], [21 ,18, 27]])
-arr2 = npy.array([[15 ,33, 24], [4 ,7, 1]])
-
-print(npy.add(arr1, arr2))
-print(npy.multiply(npy.add(arr1, arr2), npy.add(arr1, arr2)))
