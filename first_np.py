052326657ac9e05747286429c58972cb2b7648e2
kaustubh0201 <55658401+kaustubh0201@users.noreply.github.com>
2020-04-09 02:39:08 +0530

    Delete first_np.py

--- first_np.py
+++
@@ -1,9 +0,0 @@
-import numpy as npy
-
-a = npy.arange(0,8, dtype = "uint16")
-
-m = npy.reshape(a, (4,2))
-
-print("This is the dimensions of the array: ", m.shape)
-print("The length of each element of the array in byte is: ", m.itemsize)
-print("The dimension of the array is:", m.ndim)
