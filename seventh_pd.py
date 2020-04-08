6551da4e18ad3b8d874ca03d0e88e20bade326d8
kaustubh0201 <55658401+kaustubh0201@users.noreply.github.com>
2020-04-09 02:39:39 +0530

    Delete seventh_pd.py

--- seventh_pd.py
+++
@@ -1,9 +0,0 @@
-import pandas as pds 
-
-datafr = pds.read_csv('/home/kaustubh/Documents/Automobile_data.csv')
-
-maximum = datafr['price'].max()
-print(maximum)
-
-lol = datafr[['company', 'price']][datafr['price'] == maximum]
-print(lol)
